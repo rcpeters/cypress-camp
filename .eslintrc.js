205b9243917fcc74c7b5b3ae355852fd427082fc
Robert Peters <info@rcpeters.com>
2022-05-17 15:46:55 -0700

    setting up prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['airbnb-base'],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    rules: {
-        'prettier/prettier': 'error',
-    },
-    plugins: ['prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  plugins: ['prettier'],
 }
